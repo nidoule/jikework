cd21117404e798b8f632c8bf87d8e9477b2a9919
nidoule <1297835498@qq.com>
2016-12-29 15:29:25 +0800

    Revert "1"
    This reverts commit 1dd7319cc22cbfae3b783db9e2248776e5b3a23e.

--- week7/test7.1/javascript/hu.js
+++
@@ -1,9 +0,0 @@
-if (window.localStorage.getItem('theme')) {
-    changeColor(window.localStorage.getItem('theme'));
-}
-function changeColor(color) {
-    document.getElementById("thr").style.backgroundColor = color;
-    
-    window.localStorage.setItem('theme', color);
-
-}
--- week8/test8-1/server/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Content-type:application/json;charset=utf-8");
-	$link = mysqli_connect('localhost','root','root','baidunews',8889);
-?>
