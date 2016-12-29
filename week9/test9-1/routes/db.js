7775dcad459bd421f2fb14120469d7ab9370f2f5
nidoule <1297835498@qq.com>
2016-12-29 15:52:11 +0800

    1
    1

---
+++ week9/test9-1/routes/db.js
@@ -0,0 +1,11 @@
+ 
+ module.exports = {
+ onelib: {
+ host: 'localhost',   // MySQL所在服务器IP
+ user: 'root',        // 用户名
+ password: 'root',        // 密码
+ database:'baidunews',   // 数据库名称
+ port: 3306,          // 端口号
+ dateStrings: true,   // 时间以字符串形式显示，否则会有类似这样的显示：Thu Apr 14 2016 00:00:00 GMT+0800 (中国标准时间) 17:20:12
+ }
+};
